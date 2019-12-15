654c7551b354c368d3bc9d819aa7db953568442e
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-12-15 16:59:08 +0300

    Use named export

--- rollup.config.js
+++ rollup.config.js
@@ -21,10 +21,12 @@ export default [
       {
         file: pkg.main,
         format: 'cjs',
+        exports: 'named'
       },
       {
         file: pkg.module,
         format: 'es',
+        exports: 'named'
       },
     ],
     plugins: [
