b8aeb4790dffe9696d1264703d634f544cdb31ba
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-12-15 14:28:37 +0300

    Remove example from repo

--- example/node.js
+++
@@ -1,7 +0,0 @@
-const PutioAPI = require('../dist/index.js')
-
-const Api = new PutioAPI({ clientID: 'OAUTH_CLIENT_ID' }).setToken('XYZ')
-
-Api.User.Info()
-  .then(res => console.log('User info received: ', res))
-  .catch(err => console.log('An error occured while fetching user info: ', err))
