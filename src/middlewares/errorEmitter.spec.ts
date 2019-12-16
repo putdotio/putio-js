82a4989d96d5df2c875c56cd5b0e73fd374e7c20
Altay Aydemir <altay.aydemir@gmail.com>
2019-12-16 19:38:23 +0100

    Add tests for errorEmitter middleware

---
+++ src/middlewares/errorEmitter.spec.ts
@@ -0,0 +1,42 @@
+import {
+  MockPutioAPIClient,
+  mockPutioAPIClientError,
+  mockPutioAPIClientResponse
+} from "../test-utils/mocks";
+import { IPutioAPIClientError, PutioAPIClientEventTypes } from "../types";
+import create from "./errorEmitter";
+
+describe("middlewares/errorEmitter", () => {
+  const API = new MockPutioAPIClient();
+  const errorEmitter = create(API);
+
+  beforeEach(jest.resetAllMocks);
+
+  describe("successful responses", () => {
+    it("does not call client.emit method", () => {
+      errorEmitter.onFulfilled(mockPutioAPIClientResponse);
+      expect(API.emit).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("failed responses", () => {
+    it("calls client.emit method with correct signature", () => {
+      const error: IPutioAPIClientError = {
+        ...mockPutioAPIClientError,
+        response: {
+          config: {},
+          data: {
+            error_type: "API_ERROR",
+            error_message: "Putio API Error"
+          },
+          headers: {},
+          status: 400,
+          statusText: "Error!"
+        }
+      };
+
+      errorEmitter.onRejected(error).catch(e => expect(e).toEqual(error))
+      expect(API.emit).toHaveBeenCalledWith(PutioAPIClientEventTypes.ERROR, error.data);
+    })
+  });
+});
