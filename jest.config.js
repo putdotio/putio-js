63894d2937fd3d9e7c8ee73f5af48f46b15e63e6
Altay Aydemir <altay.aydemir@gmail.com>
2020-01-18 21:47:22 +0100

    configure coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 const { defaults } = require('jest-config')
 
 module.exports = {
-  transform: {
-    '.(ts|tsx)': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  preset: 'ts-jest',
+  testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, 'dist'],
 }
