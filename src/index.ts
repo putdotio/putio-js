3cae7f190acedb9c9cdabced4df8969246295882
Altay Aydemir <altay.aydemir@gmail.com>
2020-04-01 00:15:32 +0200

    Add isPutioAPIError type guard

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default } from './client'
 export * from './types'
 export * from './test-utils/mocks'
+export { isPutioAPIError } from './utils'
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,8 @@
+import { IPutioAPIClientErrorData } from './types'
+
 export const identity = (arg: any) => arg
+
+export const isPutioAPIError = (
+  input: unknown,
+): input is IPutioAPIClientErrorData =>
+  typeof input === 'object' && Object.keys(input).includes('error_type')
