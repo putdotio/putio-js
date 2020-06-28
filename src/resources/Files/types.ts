58b7efd95f0f5d2a542b24f55fa2778f2192caec
Altay Aydemir <altay.aydemir@gmail.com>
2020-06-28 13:47:26 +0200

    Add types for Files resource

--- src/resources/Files/types.ts
+++ src/resources/Files/types.ts
@@ -9,6 +9,24 @@ export type FileType =
   | 'TEXT'
   | 'SWF'
 
+export interface IFile extends Record<string, any> {
+  id: number
+  parent_id: number
+  name: string
+  size: number
+  file_type: FileType
+  content_type: string
+  extension: string
+  crc32: string
+  created_at: string
+}
+
 export interface IGetStartFromResponse {
   start_from: number
+}
+
+export interface ISearchResponse {
+  files: IFile[]
+  total: number
+  cursor: string
 }
