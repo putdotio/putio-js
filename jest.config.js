21d3fa7be083cbf4884be280154fb5bbca75237e
Altay Aydemir <altay.aydemir@gmail.com>
2020-07-10 22:54:11 +0200

    Collect coverage logically

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ const { defaults } = require('jest-config')
 module.exports = {
   preset: 'ts-jest',
   testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, 'dist'],
+  collectCoverageFrom: ['./src/middlewares/**', './src/utils/**'],
 }
